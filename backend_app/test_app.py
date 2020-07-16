565ba72ca630613bb45e43eaffe8bed7d1570b3d
AliMalik9599 <amalik36@jhu.edu>
2020-07-16 15:12:17 +0400

    Added test for emotion with standard img...successful

--- backend_app/test_app.py
+++ backend_app/test_app.py
@@ -4,16 +4,24 @@ from PIL import Image
 import numpy
 from io import BytesIO
 from pathlib import Path
-import os, sys, subprocess
+import os
+import sys
+import subprocess
 import threading
 from io import StringIO
 import cv2
+from app import emo_recognition
+
 
 class Test_app(unittest.TestCase):
     def test_stream_video(self):
         with app.app.test_client() as c:
             a = c.get('/music/happy')
         assert a is not None
-    
+
+    def test_cnn(self):
+        self.assertEqual(emo_recognition('./39.jpg'), 'Surprise')
+
+
 if __name__ == "__main__":
     unittest.main()
